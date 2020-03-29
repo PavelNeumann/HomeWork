5106d91aeec152f7309a14f081293fa86dfcbeed
PavelNeumann <55145026+PavelNeumann@users.noreply.github.com>
2020-03-29 20:15:31 +0200

    Update index.php

--- AnalyzátorSítě/index.php
+++ AnalyzátorSítě/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE HTML>
+R<!DOCTYPE HTML>
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
@@ -10,6 +10,16 @@
                 <h1>Simple IP Calculator</h1>
                 <tr><td>IP address</td><td><input type="text" placeholder="xxx.xxx.xxx.xxx" name="IP_value" id="" maxlength="15" /></td>
                     <td>/</td><td><input name="prefix_value" id=""maxlenght="2" placeholder="xx"/></td></tr>
+                <tr><td>Number of Subnets</td><td><input list="prefix" name="prefix_value" id=""maxlenght="2" placeholder="xx"/></td></tr>
+                <datalist id="prefix">
+                    <option value="24">
+                    <option value="25">
+                    <option value="26">
+                    <option value="27">
+                    <option value="28">
+                    <option value="29">
+                    <option value="30">
+                </datalist>
                 <tr><td><input type="submit" name="submit" value="submit" /></td></tr>
                 <?php
                 If (isset($_POST[submit])) {
@@ -25,28 +35,28 @@
                             if ($prefix_value == "32" or $prefix_value == "31") {
                                 echo "These prefixes are useless!";
                             } else {
-                                /* výpočet bitů, adres, a hostů */
+                                /* vĂ˝poÄŤet bitĹŻ, adres, a hostĹŻ */
                                 $bits = "32" - $prefix_value;
                                 $adress = "2" ** $bits;
                                 $hosts = $adress - "2";
-                                /* výpočet masky */
+                                /* vĂ˝poÄŤet masky */
                                 $last_octet = "256" - $adress;
                                 $mask = array("255", "255", "255", $last_octet);
                                 $mask_bin = array(decbin($mask[0]), decbin($mask[1]), decbin($mask[2]), decbin($mask[3]));
-                                /* výpočet Networku */
+                                /* vĂ˝poÄŤet Networku */
                                 $y = "8" - $bits;
-                                $x = substr_replace (str_pad( decbin($values[3]),8,0,STR_PAD_LEFT), "00000000", $y);
+                                $x = substr_replace(str_pad(decbin($values[3]), 8, 0, STR_PAD_LEFT), "00000000", $y);
                                 $network_octet = bindec(substr($x, 0, 8));
-                                /* první a poslední host */
+                                /* prvnĂ­ a poslednĂ­ host */
                                 $first = $network_octet + 1;
                                 $last = $network_octet + $hosts;
                                 /* Broadcast */
-                                $broadcast_octet =  $last + 1;
+                                $broadcast_octet = $last + 1;
                                 echo "Your Resoults:";
                                 echo "<br>";
-                                echo "Yout IP:",$values[0], ".", $values[1], ".", $values[2], ".", $values[3],"/",$prefix_value;
+                                echo "Yout IP:", $values[0], ".", $values[1], ".", $values[2], ".", $values[3], "/", $prefix_value;
                                 echo "<br>";
-                                echo "IP in binar:", str_pad(decbin($values[0]),8,0,STR_PAD_LEFT), ".", str_pad(decbin($values[1]),8,0,STR_PAD_LEFT), ".", str_pad(decbin($values[2]),8,0,STR_PAD_LEFT), ".", str_pad(decbin($values[3]),8,0,STR_PAD_LEFT);
+                                echo "IP in binar:", str_pad(decbin($values[0]), 8, 0, STR_PAD_LEFT), ".", str_pad(decbin($values[1]), 8, 0, STR_PAD_LEFT), ".", str_pad(decbin($values[2]), 8, 0, STR_PAD_LEFT), ".", str_pad(decbin($values[3]), 8, 0, STR_PAD_LEFT);
                                 echo "<br>";
                                 echo "Mask:", "255", ".", "255", ".", "255", ".", $last_octet;
                                 echo "<br>";
